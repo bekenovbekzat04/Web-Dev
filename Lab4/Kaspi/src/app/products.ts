758eeb26bcc16ede224c5c6d1f7fd2829b72dfde
bekenovbekzat04 <93797483+bekenovbekzat04@users.noreply.github.com>
2023-02-23 00:38:57 +0600

    Add files via upload

---
+++ Lab4/Kaspi/src/app/products.ts
@@ -0,0 +1,138 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  image: string;
+  link: string;
+  rating: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Apple MacBook Air 13 MGN63 Laptop Grey',
+    price: 480550,
+    description:
+      'A small chip. A grand breakthrough is the first chip designed specifically for Mac. Amazingly, the system on the Apple M1 chip accommodates 16 billion transistors and combines the central and graphics processors, the Neural Engine system, I/O controllers and many other components. The Apple M1 chip allows you to use unique technologies on a Mac and provides incredible performance combined with the industrys best energy efficiency. This is not just another step for Mac — it is a fundamentally new level of capabilities. 8‑core processor.',
+    link: 'https://kaspi.kz/shop/p/apple-macbook-air-13-mgn63-seryi-100797845/?ref=shared_link&c=750000000#!/item',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h65/h0f/33125684084766/apple-macbook-air-2020-13-3-mgn63-seryj-100797845-1-Container.jpg',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+  },
+  {
+    id: 2,
+    name: 'Laptop Acer Nitro 5 AN515-57 NH.QEKER.004 black',
+    price: 389990,
+    description:
+      'Diagonal: 15.6" - 39.62 cm / Processor: Intel ® Core ™ i5 / Operating System: DOS / RAM: 16 GB / Hard Disk Type: SSD / Storage Capacity: 512 GB / Video Card Series: NVIDIA GeForce GTX / Video Adapter Chipset: 1650 / Video Adapter Memory: 4 GB',
+    link: 'https://kaspi.kz/shop/p/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028/?c=750000000&m=Sulpak&ref=shared_link#!/item',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h2e/hc4/67236399185950/acer-nitro-5-an515-57-nh-qeker-004-chernyi-108194028-1.jpg',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+  },
+  {
+    id: 3,
+    name: 'Lenovo IdeaPad 3 15ITL6 82H8005GRK Laptop Silver',
+    price: 164990,
+    description:
+      "The IdeaPad 3i Gen 6 (15) laptop will help you cope with any task thanks to the unprecedented speed and intelligent capabilities of Intel ® Core ™ processors of the 11th generation, and the built-in Intel ® graphics card Iris ® Xe will provide high image quality. The laptop's power is enough to run the most complex applications and AAA games. At the same time, its intelligent functions will ensure optimal performance of the latest processors that the device is equipped with — everything for efficient work and pleasant entertainment at home.",
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h8f/h3a/66993674190878/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705-1.jpg',
+    link: 'https://kaspi.kz/shop/p/lenovo-ideapad-3-15itl6-82h8005grk-serebristyi-108090705/?c=750000000&m=PrimeComputers&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+  },
+  {
+    id: 4,
+    name: 'Apple Iwatch SE 2nd Gen 40mm Gold Smart Watch',
+    price: 169880,
+    description:
+      'The case is in three stylish colors. Powerful sensors for information about your health and activity. Innovative security features. Convenient ways to stay connected. Faster dual-core processor for increased performance. The Apple Watch SE is full of virtues, and now it is an even better purchase than ever.',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h97/h46/62711073407006/apple-watch-se-2nd-generation-40-mm-zolotistyj-106362759-1.jpg',
+    link: 'https://kaspi.kz/shop/p/apple-watch-se-2nd-gen-40-mm-zolotistyi-106362759/?c=750000000&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+  },
+  {
+    id: 5,
+    name: 'Xbox Series S Game Console white',
+    price: 164152,
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h3b/hdd/33118956126238/xbox-series-s-belyj-100793537-1-Container.jpg',
+    description:
+      'The next generation of games brings with it our largest digital library for our smallest Xbox. With more dynamic game worlds, faster downloads, and the addition of the Xbox Game Pass (sold separately), the Xbox Series S fully digital console is the best deal in the gaming world. Games created using the Xbox Series X|S development kit demonstrate significantly reduced loading time and stunning visual effects at speeds up to 120 fps.',
+    link: 'https://kaspi.kz/shop/p/xbox-series-s-belyi-100793537/?c=750000000&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+  },
+  {
+    id: 6,
+    name: 'Apple iPhone 14 Pro Max 256Gb Purple Smartphone',
+    price: 692000,
+    description:
+      'A new magical way to interact with the iPhone. An impressive safety feature that helps save lives. The advanced main camera is 48 MP. And the display is up to twice as bright in sunlight.◊Footnote with reference to legal information All this is under the control of the most powerful iPhone chip.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000&sr=3&qid=7d113d5337cd87f447454df3ba3b6006&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h00/h18/62948780834846/apple-iphone-14-pro-max-128gb-fioletovyj-106363342-1.jpg',
+  },
+  {
+    id: 7,
+    name: 'Apple iPhone 13 128Gb Smartphone Black',
+    price: 372420,
+    description:
+      'The Apple iPhone 13 received a 6.1—inch Super Retina XDR display, which features an incredibly high pixel density - photos, videos and text look strikingly clear. And thanks to the reduced area of the True Depth camera, there is now more space for the image on the display. Apple has developed a completely new camera layout and deployed the lenses at 45 degrees. Thanks to this, the best system of two cameras with an enlarged matrix of a wide-angle camera fit inside the case. In addition, space was freed up for an optical image stabilization system with a matrix shift, and the speed of the matrix on an ultra-wide-angle camera increased.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-13-128gb-chernyi-102298404/?c=750000000&sr=1&qid=7d113d5337cd87f447454df3ba3b6006&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h49/hc0/46392662523934/apple-iphone-13-128gb-cernyj-102298404-1-Container.jpg',
+  },
+  {
+    id: 8,
+    name: 'Apple iPhone 12 64Gb Smartphone Black',
+    price: 321720,
+    description:
+      'Incredibly durable Ceramic Shield front panel. Gorgeous bright OLED display. A powerful chip. And a two-camera system. All this is in the Apple iPhone 12. The shape without rounded edges not only provided incredible strength, but also made it possible to make the display larger. When making the front panel of a smartphone, ceramic nanocrystals are added to the glass matrix, which are stronger than many known metals. Dual ion exchange technology provides excellent protection against cracks, scratches and other minor damages. The frame made of aluminum used in the aerospace industry is complemented by a polished glass back panel. The iPhone 12 has become more compact, thinner and lighter than the iPhone 11, but the size of its display has not changed.',
+    link: 'https://kaspi.kz/shop/p/apple-iphone-12-64gb-chernyi-100656839/?c=750000000&sr=24&qid=7d113d5337cd87f447454df3ba3b6006&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hfa/h97/33279114575902/apple-iphone-12-64gb-cernyj-100656839-1-Container.jpg',
+  },
+  {
+    id: 9,
+    name: 'Sony PlayStation 5 Game Console white',
+    price: 330000,
+    description:
+      'With Sony PlayStation 5, you will experience the true power of the game Lightning-fast download speed thanks to a super-fast SSD drive, an incredible immersion effect thanks to tactile feedback, adaptive trigger buttons and 3D sound, as well as amazing new generation games for PlayStation 5. Incredible speed Experience the power of a new CPU, GPU and SSD with a special built-in input and output system, which turn the ideas about the possibilities of the PlayStation. Built-in input and output system A special integrated input and output system on the PS5 console allows developers to receive data from an SSD drive so quickly that game creators have previously unattainable opportunities',
+    link: 'https://kaspi.kz/shop/p/sony-playstation-5-belyi-100746577/?c=750000000&sr=1&qid=18ed94d2c50dfe1e890d0850aaf9beb3&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hd1/hbe/49348562681886/sony-playstation-5-belyj-100746577-1-Container.jpg',
+  },
+  {
+    id: 10,
+    name: 'Apple Earpods Pro 2nd generation headphones white',
+    price: 119480,
+    description:
+      'Up to twice as effective active noise reduction with AirPods Pro.1 As well as adaptive transparency and personal spatial audio with dynamic tracking of head movements for immersive sound.2 Now there are earbuds in several sizes at once (XS, S, M, L), and the listening time has been increased to 6 hours without recharging.³',
+    link: 'https://kaspi.kz/shop/p/apple-airpods-pro-2nd-generation-belyi-106362968/?c=750000000&sr=3&qid=eba385129d247d8c818904093ed0a31f&ref=shared_link#!/item',
+    rating:
+      'https://static.vecteezy.com/system/resources/previews/004/256/658/original/five-star-customer-product-ratings-review-flat-icons-for-apps-and-websites-illustration-of-five-golden-yellow-stars-in-a-row-isolated-in-a-white-background-concepts-for-ratings-customers-review-free-vector.jpg',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hba/hf8/62281477259294/apple-airpods-pro-2nd-generation-belyj-106362968-1.jpg',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
